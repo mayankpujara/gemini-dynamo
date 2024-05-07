eb7081277a8e7a61d6a94a2e042a8690a0f9abf9
mayankpujara <mayankpujara25@gmail.com>
2024-05-08 01:14:15 +0530

    Create services folder | Functionalities into a class | Logging for verbosity

--- backend/main.py
+++ backend/main.py
@@ -1,8 +1,8 @@
 from fastapi import FastAPI
 from pydantic import BaseModel, HttpUrl
-from langchain_community.document_loaders import YoutubeLoader
-from langchain.text_splitter import RecursiveCharacterTextSplitter
 from fastapi.middleware.cors import CORSMiddleware
+
+from services.genai import YoutubeProcessor
 
 class VideoAnalysisRequest(BaseModel):
     youtube_link: HttpUrl
@@ -23,24 +23,12 @@ app.add_middleware(
 @app.post("/analyse_video")
 def analyse_video(request:VideoAnalysisRequest):
 
-    loader = YoutubeLoader.from_youtube_url(str(request.youtube_link), add_video_info=True)
-    docs = loader.load()
+    processor = YoutubeProcessor()
+    result = processor.retrive_youtube_documents(str(request.youtube_link), verbose=True)
 
-    print(f"On Loading: {type(docs)}")
-    text_splitter = RecursiveCharacterTextSplitter(chunk_size=100, chunk_overlap = 0)
-    result = text_splitter.split_documents(docs)
-
-    print(f"{type(docs)}")
-    author = result[0].metadata['author']
-    length = result[0].metadata['length']
-    title = result[0].metadata['title']
-    total_size = len(result)
 
     return {
-        "author": author,
-        "length": length,
-        "title": title,
-        "total_size": total_size,
+        "result": result
     }
 
 @app.get("/root")
