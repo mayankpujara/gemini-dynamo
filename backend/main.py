5abcf755e44009682620bcb774705fc1f627f3f9
mayankpujara <mayankpujara25@gmail.com>
2024-05-09 00:05:16 +0530

    Created the GeminiProcessor Class | Integrated the summarizing method with main.py

--- backend/main.py
+++ backend/main.py
@@ -2,7 +2,7 @@ from fastapi import FastAPI
 from pydantic import BaseModel, HttpUrl
 from fastapi.middleware.cors import CORSMiddleware
 
-from services.genai import YoutubeProcessor
+from services.genai import (YoutubeProcessor, GeminiProcessor)
 
 class VideoAnalysisRequest(BaseModel):
     youtube_link: HttpUrl
@@ -26,9 +26,15 @@ def analyse_video(request:VideoAnalysisRequest):
     processor = YoutubeProcessor()
     result = processor.retrive_youtube_documents(str(request.youtube_link), verbose=True)
 
+    genai_processor = GeminiProcessor(
+        model_name= "gemini-pro",
+        project = "radicalx-gemini-dynamo"
+    )
+
+    summary = genai_processor.generate_document_summary(result, verbose = True)
 
     return {
-        "result": result
+        "summary": summary
     }
 
 @app.get("/root")
